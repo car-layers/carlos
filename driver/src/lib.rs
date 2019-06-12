3d79d2c13863082ecb22bbb4f5f7218a565e806e
Daniel Olano <danielolanoc@gmail.com>
2019-06-12 09:17:35 +0200

    Connect driver and pax with webrtc

--- driver/src/lib.rs
+++ driver/src/lib.rs
@@ -5,13 +5,13 @@ extern crate web_sys;
 use carlos::presence::{self, Role};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{Event, WebSocket};
+use web_sys::{console, Event, MessageEvent, WebSocket};
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     let p = presence::who(Role::Driver, "Driver");
     let socket = WebSocket::new("ws://192.168.0.3:3000")?;
-    let cb = Closure::once_into_js(move |e: Event| {
+    let on_conn = Closure::once_into_js(move |e: Event| {
         let mut p = presence::pack(p.into_inner()).unwrap();
         e.target()
             .expect("event has no target")
@@ -19,6 +19,10 @@ pub fn main() -> Result<(), JsValue> {
             .send_with_u8_array(&mut p)
             .unwrap();
     });
-    socket.add_event_listener_with_callback("open", cb.unchecked_ref())?;
+    let on_msg = Closure::once_into_js(move |e: MessageEvent| {
+        console::log_1(&e.data());
+    });
+    socket.add_event_listener_with_callback("open", on_conn.unchecked_ref())?;
+    socket.add_event_listener_with_callback("message", on_msg.unchecked_ref())?;
     Ok(())
 }
--- presence/src/main.rs
+++ presence/src/main.rs
@@ -1,19 +1,28 @@
 extern crate ws;
 
 use carlos::presence;
+use std::cell::Cell;
+use std::rc::Rc;
 use ws::{listen, Error, ErrorKind, Message};
 
 fn main() {
-    listen("0.0.0.0:3000", |_out| {
-        println!("connected");
-        move |msg| {
-            let data = match msg {
-                Message::Binary(data) => data,
-                Message::Text(_) => return Err(Error::new(ErrorKind::Protocol, "Wrong data type")),
-            };
-            let role = presence::from(data)
+    let vehicle_online = Rc::new(Cell::new(false));
+    listen("0.0.0.0:3000", |out| {
+        let online = vehicle_online.clone();
+        move |msg: Message| {
+            let role = presence::from(msg.into_data())
                 .map_err(|_| Error::new(ErrorKind::Internal, "Couldn't parse capnp message"))?;
             println!("{:?}", role);
+            match role {
+                presence::Role::Vehicle => {
+                    online.set(true);
+                }
+                presence::Role::Driver => {
+                    if online.get() {
+                        out.send("online!")?;
+                    }
+                }
+            }
             Ok(())
         }
     })
